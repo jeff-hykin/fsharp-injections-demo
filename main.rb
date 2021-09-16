8178306cde6ab7db161780e45b2a910c8136ab47
Jeff Hykin <jeff.hykin@gmail.com>
2021-09-16 18:06:14 -0500

    make more readable

--- main.rb
+++ main.rb
@@ -5,27 +5,24 @@ g = Grammar.new name: "F# Templates",
                 ending: "",
                 injectionSelector: "L:source.fsharp - (comment, string.quoted.double.fsharp, string.quoted.triple.fsharp)"
 
-def P(*a) Pattern.new(*a) end
-def PR(*a) PatternRange.new(*a) end
-def I(i,l)
+def format_str(pat, with)    
+    start = Pattern.new tag_as: "punctuation.section.attribute.begin",
+                        match: Pattern.new(/[ ._]/).then(match: pat, tag_as: "variable.fsharp").then(/ *(?:(?=\$)|$)/)
     
-    s = P tag_as: "punctuation.section.attribute.begin",
-          match: P(/[ ._]/).then(match: i, tag_as: "variable.fsharp").then(/ *(?:(?=\$)|$)/)
+    stop = Pattern.new tag_as: "punctuation.section.attribute.end",
+                       match: /"""/
     
-    e = P tag_as: "punctuation.section.attribute.end",
-          match: /"""/
+    inner = PatternRange.new tag_content_as: "template.fsharp.html",
+                             start_pattern: /\$"""/,
+                             end_pattern: /(?=""")/,
+                             includes: [with]
     
-    n = PR tag_content_as: "template.fsharp.html",
-           start_pattern: /\$"""/,
-           end_pattern: /(?=""")/,
-           includes: [l]
+    context = inner, "source.fsharp"
     
-    c = n, "source.fsharp"
-    
-    PR start_pattern: s,
-       end_pattern: e,
-       includes: c
+    PatternRange.new start_pattern: start,
+                     end_pattern: stop,
+                     includes: context
 end
 
-g[:$initial_context] = I( /html|svg/, "text.html.derivative" ), I( /sql/, "source.sql" )
+g[:$initial_context] = format_str(/html|svg/, "text.html.derivative"), format_str(/sql/, "source.sql")
 g.save_to syntax_name: "o"
