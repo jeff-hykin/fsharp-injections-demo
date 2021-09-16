60ec0ca7f04ed5bf124027e5c388a22c1c78cb2c
Jeff Hykin <jeff.hykin@gmail.com>
2021-09-16 17:42:27 -0500

    minimize

---
+++ main.rb
@@ -0,0 +1,35 @@
+require 'ruby_grammar_builder'
+
+g = Grammar.new name: "F# Templates",
+                scope_name: "template.fsharp.injection",
+                ending: "",
+                injectionSelector: "L:source.fsharp - (comment, string.quoted.double.fsharp, string.quoted.triple.fsharp)"
+
+def P(*a); Pattern.new(*a) end
+def PR(*a); PatternRange.new(*a) end
+def I(i,l)
+    
+    s = P tag_as: "punctuation.section.attribute.begin",
+          match: P(/[ ._]/).then(match: i, tag_as: "variable.fsharp").then(/ *(?:(?=\$)|$)/)
+    
+    e = P tag_as: "punctuation.section.attribute.end",
+          match: /"""/
+    
+    n = PR tag_content_as: "template.fsharp.html",
+           start_pattern: /\$"""/,
+           end_pattern: /(?=""")/,
+           patterns: [l]
+    
+    c = n, "source.fsharp"
+    
+    PR start_pattern: s,
+       end_pattern: e,
+       includes: c
+end
+
+g[:$initial_context] = [
+    I( /html|svg/, "text.html.derivative" ),
+    I( /sql/, "source.sql" ),
+]
+
+g.save_to syntax_name: "o"
--- main/test.rb
+++
@@ -1,4 +0,0 @@
-require 'ruby_grammar_builder'
-
-puts lookAheadFor(/$/).to_yaml
-puts lookAheadFor("$").to_yaml
